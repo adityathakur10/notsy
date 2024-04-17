d7000e4394785408aaeff0b67312869891cfe62d
Arault-Govinda <arooult@gmail.com>
2024-04-17 18:57:49 +0530

    Responsive

---
+++ public/javascripts/Homepage.js
@@ -0,0 +1,40 @@
+// Pdf upload k liye
+document.querySelector("#pdf-upload").addEventListener("click", function(){
+    document.querySelector("#uploadform input").click();
+})
+
+document.querySelector("#uploadform input").addEventListener("change", function(){
+    document.querySelector("#uploadform").submit();
+})
+
+
+// Notes section k liye
+
+var arr =[
+    {icon:"/images/edit.png", name:"Computer Networks", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."},
+    {icon:"/images/edit.png", name:"DSA", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."},
+    {icon:"/images/edit.png", name:"Python", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."},
+    {icon:"/images/edit.png", name:"Numerical Analysis", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."},
+    {icon:"/images/edit.png", name:"Operating System", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."},
+    {icon:"/images/edit.png", name:"Digital System", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."},
+    {icon:"/images/edit.png", name:"Analog Electronics", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."},
+    {icon:"/images/edit.png", name:"Advanced Algo", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."},
+    {icon:"/images/edit.png", name:"AI", des:"Lorem ipsum dolor sit amet,dunt ut labore et dolore magna aliqua."}
+]
+
+function enterNote(){
+    var clutter = "";
+    arr.forEach(function(elem,index){
+        clutter += `<div class="box">
+        <img src="${elem.icon}" alt="">
+        <h1>
+            ${elem.name}
+        </h1>
+        <p>${elem.des}</p>
+    </div>`;
+    })
+
+    document.querySelector(".notes-container").innerHTML = clutter;
+}
+
+enterNote();
