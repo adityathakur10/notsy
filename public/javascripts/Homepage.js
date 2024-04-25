fddf90d78e44cd9c57513f3bfa8e1bb455aad1dd
Arault-Govinda <arooult@gmail.com>
2024-04-25 19:50:49 +0530

    login popup

--- public/javascripts/Homepage.js
+++ public/javascripts/Homepage.js
@@ -40,3 +40,27 @@ function enterNote() {
 }
 
 enterNote();
+
+
+//login 
+const wrapper = document.querySelector('.wrapper')
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link')
+const btnPopup = document.querySelector('.login-button')
+const closePopup = document.querySelector('.icon-close')
+
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+})
+
+btnPopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+})
+
+closePopup.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+})
